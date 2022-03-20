7c8b5a7bfc55cbad74c348f7f928fc4d063f345f
pauldipasquale <pauldipasquale756@gmail.com>
2022-03-20 10:26:18 -0700

    homepage

--- main.py
+++ main.py
@@ -10,22 +10,12 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/login/')
+def login():
+    return render_template("login.html")
+
 
 # connects /kangaroos path to render kangaroos.html
-@app.route('/kangaroos/')
-def kangaroos():
-    return render_template("kangaroos.html")
-
-
-@app.route('/walruses/')
-def walruses():
-    return render_template("walruses.html")
-
-
-@app.route('/hawkers/')
-def hawkers():
-    return render_template("hawkers.html")
-
 
 @app.route('/stub/')
 def stub():
