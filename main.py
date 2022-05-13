6a66d7674e7ea5915e4dc8103089ddbc818e2800
pauldipasquale <pauldipasquale756@gmail.com>
2022-05-13 10:59:21 -0700

    More Login setup

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 # import "packages" from flask
-from flask import Flask, render_template
+from flask import render_template, redirect, request, url_for
+from flask_login import login_required
+
+from __init__ import app, login_manager
 from cruddy.app_crud import app_crud
+from cruddy.app_crud_api import app_crud_api
+
+from cruddy.login import login, logout, authorize
+
+app.register_blueprint(app_crud_api)
+app.register_blueprint(app_crud)
+
+
 from __init__ import app
 
-app.register_blueprint(app_crud)
 # create a Flask instance
-app = Flask(__name__)
-
 
 # connects default URL to render index.html
 
@@ -14,13 +22,59 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@login_manager.unauthorized_handler
+def unauthorized():
+    """Redirect unauthorized users to Login page."""
+    global next_page
+    next_page = request.endpoint
+    return redirect(url_for('main_login'))
 
 
-# connects /kangaroos path to render kangaroos.html
+# if login url, show phones table only
+@app.route('/login/', methods=["GET", "POST"])
+def main_login():
+    # obtains form inputs and fulfills login requirements
+    global next_page
+    if request.form:
+        email = request.form.get("email")
+        password = request.form.get("password")
+        if login(email, password):
+            if next_page is None:
+                return redirect(url_for('index'))
+            else:
+                temp = next_page
+                next_page = None
+                return redirect(url_for(temp))
 
-@app.route('/stub/')
-def stub():
-    return render_template("stub.html")
+    # if not logged in, show the login page
+    return render_template("login.html")
+
+
+# if login url, show phones table only
+@app.route('/logout/', methods=["GET", "POST"])
+@login_required
+def main_logout():
+    logout()
+    return redirect(url_for('index'))
+
+
+@app.route('/authorize/', methods=["GET", "POST"])
+def main_authorize():
+    error_msg = ""
+    # check form inputs and creates user
+    if request.form:
+        # validation should be in HTML
+        user_name = request.form.get("user_name")
+        email = request.form.get("email")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")  # password should be verified
+        if password1 == password2:
+            if authorize(user_name, email, password1):
+                return redirect(url_for('main_login'))
+        else:
+            error_msg = "Passwords do not match"
+    # show the auth user page if the above fails for some reason
+    return render_template("authorize.html", error_msg=error_msg)
 
 @app.route('/calen/')
 def calen():
