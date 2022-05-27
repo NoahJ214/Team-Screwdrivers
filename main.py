bfb1b362902d054c009ca52907df566b5db4924a
pauldipasquale <pauldipasquale756@gmail.com>
2022-05-27 13:52:55 -0700

    db

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ from flask_login import login_required
 from __init__ import app, login_manager
 from cruddy.app_crud import app_crud
 from cruddy.app_crud_api import app_crud_api
+from cruddy.app_notes import app_notes
+
 
 from cruddy.login import login, logout, authorize
 
 app.register_blueprint(app_crud_api)
 app.register_blueprint(app_crud)
+app.register_blueprint(app_notes)
 
 
 from __init__ import app
